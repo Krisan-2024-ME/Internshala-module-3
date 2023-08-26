a2cbf5bca539e05de265a12879b69df0fee87acf
Krisan Kumar Gupta <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-08-27 00:56:12 +0530

    Add files via upload

---
+++ first c++ code.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<"My name is Rahul."<<endl;
+    //cout<<"I am 20 years old.";
+    cout<<"My name is Rahul.\n";
+    cout<<"I am 20 year old.";
+    return 0;
+}
